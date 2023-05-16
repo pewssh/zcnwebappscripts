77f8b494876764cb5f9a8e2a90c934c93594bbee
zus <zus@test.com>
2023-05-16 19:57:02 +0000

    Update Image tag in script

--- script/blimp.sh
+++ script/blimp.sh
@@ -12,7 +12,7 @@ BLIMP_DOMAIN=blimpdomain
 WALLET_ID=0chainwalletid
 WALLET_PUBLIC_KEY=0chainwalletpublickey
 WALLET_PRIVATE_KEY=0chainwalletprivatekey
-DOCKER_IMAGE=staging-6e3e0ac8
+DOCKER_IMAGE=staging-e305af20
 
 sudo apt update
 sudo apt install -y unzip curl containerd docker.io jq
