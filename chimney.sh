c4e3957d758d4c084566daf0ef135c9d95b52884
Manohar Reddy <b.manu199@gmail.com>
2023-08-10 10:21:52 +0200

    Update chimney.sh

--- chimney.sh
+++ chimney.sh
@@ -36,7 +36,7 @@ export PROJECT_ROOT_HDD=/var/0chain/blobber/hdd
 
 
 sudo apt update -qq
-sudo apt install -qqy unzip curl containerd docker.io ntp systemd
+sudo apt install -qqy unzip curl containerd docker.io systemd systemd-timesyncd
 
 sudo systemctl enable ntp
 sudo systemctl unmask systemd-timesyncd
