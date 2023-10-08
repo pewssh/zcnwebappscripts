594de372a2262966b8d9bc1b04761b84b9fc7bbc
Manohar Reddy <b.manu199@gmail.com>
2023-10-08 12:37:17 +0200

    use `sprint-1.11` image

--- chimney.sh
+++ chimney.sh
@@ -243,7 +243,7 @@ services:
     restart: "always"
 
   validator:
-    image: 0chaindev/validator:staging
+    image: 0chaindev/validator:sprint-1.11
     environment:
       - DOCKER= true
     volumes:
@@ -257,7 +257,7 @@ services:
     restart: "always"
 
   blobber:
-    image: 0chaindev/blobber:staging
+    image: 0chaindev/blobber:sprint-1.11
     environment:
       DOCKER: "true"
       DB_NAME: blobber_meta
