1ee113062587ac583c78972ff4624b9a202db828
shahnawaz-creator <msac8225@gmail.com>
2023-10-27 18:42:47 +0200

    updated delegate wallet script

--- 0chain/generate_delegate_wallet.sh
+++ 0chain/generate_delegate_wallet.sh
@@ -6,6 +6,7 @@ set -e
 # setup variables
 ############################################################
 export PROJECT_ROOT="/var/0chain" # /var/0chain
+mkdir -p $PROJECT_ROOT
 
 echo -e "\n\e[93m===============================================================================================================================================================================
                                                                 Installing some pre-requisite tools on your server
@@ -23,9 +24,8 @@ pushd ${PROJECT_ROOT} > /dev/null;
     #Delegate Wallet Input
     if [[ -f delegate_wallet.json ]] ; then
         CLIENTID=$( jq -r .client_id delegate_wallet.json )
-        echo "Delegate wallet already exists with ID ${CLIENTID}"
     else
-        echo "Creating new delegate wallet."
+        echo "\e[32m Creating new delegate wallet. \e[23m \e[0;37m"
         if [[ -f bin/zwallet ]] ; then
             echo "zwallet binary already present"
         else
@@ -34,15 +34,17 @@ pushd ${PROJECT_ROOT} > /dev/null;
                 echo "Ubuntu 18 is not supported"
                 exit 1
             elif [[ ${ubuntu_version} -eq 20 || ${ubuntu_version} -eq 22 ]]; then
-                curl -L "https://github.com/0chain/zcnwebappscripts/raw/as-deploy/0chain/artifacts/zwallet-binary.zip" -o /tmp/zwallet-binary.zip
+                curl -L "https://github.com/0chain/zcnwebappscripts/raw/add/zwallet/0chain/artifacts/zwallet-binary.zip" -o /tmp/zwallet-binary.zip
                 sudo unzip -o /tmp/zwallet-binary.zip && rm -rf /tmp/zwallet-binary.zip
-                sudo cp -rf zwallet-binary/* ${PROJECT_ROOT}/bin
+                mkdir bin
+                sudo cp -rf zwallet-binary/* ${PROJECT_ROOT}/bin/
                 sudo rm -rf zwallet-binary
             else
                 echo "Didn't found any Ubuntu version with 20/22."
             fi
         fi
-        ./bin/zwallet create-wallet --wallet delegate_wallet.json
+        ./bin/zwallet create-wallet --wallet delegate_wallet.json --configDir .
+        CLIENTID=$( jq -r .client_id delegate_wallet.json )
     fi
-
+    echo "Delegate wallet ID: ${CLIENTID}"
 popd > /dev/null;
