f9fe3c2ae0a2e97f3ac633c80bf8ecea44110d19
shahnawaz-creator <msac8225@gmail.com>
2023-11-07 16:44:47 +0100

    updated delegate wallet script

--- 0chain/generate_delegate_wallet.sh
+++ 0chain/generate_delegate_wallet.sh
@@ -90,36 +90,60 @@ pushd ${PROJECT_ROOT} > /dev/null;
 popd > /dev/null;
 
 echo -e "\n\e[93m===============================================================================================================================================================================
-                                                                            Verifying wallets for initial stats.
+                                                                            Verifying wallets in initial stats.
 ===============================================================================================================================================================================  \e[39m"
 pushd ${PROJECT_ROOT} > /dev/null;
 
     sed -i "s/10000000000/10000000000000/g" ./initial_states.yaml
+    if grep "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d3" initial_states.yaml; then
+      head -n -2 initial_states.yaml
+    fi
     if ! grep "f1d14699ccad97ca893a635e68e128b0717f8a1aab1a071db6b40935cbfce90c" initial_states.yaml; then
         cat <<EOF >>initial_states.yaml
     - id: f1d14699ccad97ca893a635e68e128b0717f8a1aab1a071db6b40935cbfce90c
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 7051ca0cf6f6157a54fa91570d2bb8ab8723b1050381b3d95b66debfdbcf5416
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 4de1553b44e4942593b96ca2ee86d543967762929bf6db9c7c65a7446984e6f1
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: c7cd30d15f713068e65c6469df38d84ec128267bba2c4067360b4d69f208c75e
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 37567fc630d9b747364678158df4fcae8d5da2c077681a592ff406c143b5c664
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: ee1a04d880f03c8f9df25f825a27526a34626dcf9bcffd5c7c182919315e899e
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 07aebb92690d3946e5f66b8088ca1fd5e8049dbf203167fc000e22a0a9ea9071
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 14b16712cc0d3d2573299a474c0e297616aaea2413709fa8d3d6fda698609142
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 5d6bb641dac8fd6d78efe64436ec4b096e2c67ba43386084fe7bce48389a8394
-      tokens: 10000000000000
+      tokens: 100000000000000
     - id: 120501bbbf5f1cbcfb939952e37ef7ff85bf0282031e2ec81edaa5f424242ae8
-      tokens: 10000000000000
+      tokens: 100000000000000
+      #owner
+    - id: 1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802
+      tokens: 200000000000000000
+      #0chain team wallet
+    - id: 65b32a635cffb6b6f3c73f09da617c29569a5f690662b5be57ed0d994f234335
+      tokens: 100000000000000000
 EOF
     else
         echo "Wallet's already added."
     fi
 
+  if $(wc -l initial_states.yaml | awk '{print $1}') != 285; then
+    echo "Initial stats file corrupted, Please contact zus team."
+    exit 1
+  fi
 popd > /dev/null;
+
+echo -e "\n\e[93m===============================================================================================================================================================================
+                                                                            Ouput Json that will be shared with zus team.
+===============================================================================================================================================================================  \e[39m"
+pushd ${PROJECT_ROOT} > /dev/null;
+    echo "Details need to be send to ZUS Team."
+    echo "{\"provider_id\":\"$( jq -r .client_id keys/b0mnode1_keys.json )\",\"domain\":\"$(cat miner/url.txt)\",\"client_id\":\"$(cat del_wal_id.txt)\"}" > send_tokens.json
+    echo
+    cat send_tokens.json
+    echo
+pushd > /dev/null;
