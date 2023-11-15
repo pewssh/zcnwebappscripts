6f82f686f8942b72332ab6f952f9e8472dc44dbe
Manohar Reddy <b.manu199@gmail.com>
2023-11-15 14:24:20 +0100

    Update blimp.sh

--- blimp.sh
+++ blimp.sh
@@ -12,7 +12,7 @@ BLIMP_DOMAIN=blimpdomain
 WALLET_ID=0chainwalletid
 WALLET_PUBLIC_KEY=0chainwalletpublickey
 WALLET_PRIVATE_KEY=0chainwalletprivatekey
-DOCKER_IMAGE=staging
+DOCKER_IMAGE=sprint-1.11-9bd91947
 
 sudo apt update
 sudo apt install -y unzip curl containerd docker.io jq
