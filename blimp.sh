4908d6063ea538b600cf423af07cbfe8d913b345
shahnawaz-creator <117025384+shahnawaz-creator@users.noreply.github.com>
2024-07-27 22:01:59 +0530

    mount zs3server access logs to server blimp.sh

--- blimp.sh
+++ blimp.sh
@@ -126,6 +126,13 @@ EOF
 
 cat <<EOF >${CONFIG_DIR}/caddyfiles/blimp.caddy
 ${BLIMP_DOMAIN} {
+	log {
+		output file /var/log/access.log {
+		roll_size 1gb
+		roll_keep 5
+		roll_keep_for 720h
+		}
+	}
 	route /minioclient/* {
 		uri strip_prefix /minioclient
 		reverse_proxy minioclient:3001
